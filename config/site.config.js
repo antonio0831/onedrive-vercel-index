a9d0dfc3795e3d11d8e40c8b05d79a0c29519505
Antonio Harris <76553040+antonio0831@users.noreply.github.com>
2022-10-15 11:58:51 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    '',
+    'Powered by Ant Cloud',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
@@ -51,9 +51,11 @@ module.exports = {
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
   links: [
-    {
-      
+     {
+      name: 'GitHub',
+      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
     },
+   
   ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
